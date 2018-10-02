ea19720b1841def8085b7de636f34e2490966f4f
Joe O'Regan <joeaoregan@yahoo.ie>
2018-10-02 12:00:38 +0100

    Lab 2 27/09/2018

---
+++ src/ait/mase/lab2/Main.java
@@ -0,0 +1,17 @@
+package ait.mase.lab2;
+
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		try {
+			new Lexer1(System.in).Input();
+			System.out.println("Lexical analysis successful");
+		} catch (ParseException e) {
+			System.out.println("Lexer Error : \n" + e.toString());
+		}
+
+	}
+
+}
