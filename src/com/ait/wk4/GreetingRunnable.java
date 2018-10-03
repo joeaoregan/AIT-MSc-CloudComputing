71fdde81aa14e900d6b78d1788e44d93a418ac07
Joe O'Regan <joeaoregan@yahoo.ie>
2018-10-03 16:41:51 +0100

    Thread example

---
+++ src/com/ait/wk4/GreetingRunnable.java
@@ -0,0 +1,37 @@
+/*
+ * Cloud Computing
+ * 03/10/2018
+ */
+package com.ait.wk4;
+
+import java.util.Date;
+
+/**
+ * A runnable that repeatedly prints a greeting.
+ */
+public class GreetingRunnable implements Runnable {
+	private static final int REPETITIONS = 10;
+	private static final int DELAY = 1000;
+
+	private String greeting;
+
+	/**
+	 * Constructs the runnable object.
+	 * 
+	 * @param aGreeting the greeting to display
+	 */
+	public GreetingRunnable(String aGreeting) {
+		greeting = aGreeting;
+	}
+
+	public void run() {
+		try {
+			for (int i = 1; i <= REPETITIONS; i++) {
+				Date now = new Date();
+				System.out.println(now + " " + greeting); // show time and message
+				Thread.sleep(DELAY);
+			}
+		} catch (InterruptedException exception) {
+		}
+	}
+}
---
+++ src/com/ait/wk4/GreetingThreadRunner.java
@@ -0,0 +1,17 @@
+/*
+ * Cloud Computing
+ * 03/10/2018
+ *  This program runs two greeting threads in parallel.
+*/
+package com.ait.wk4;
+
+public class GreetingThreadRunner {
+	public static void main(String[] args) {
+		GreetingRunnable r1 = new GreetingRunnable("Hello");
+		GreetingRunnable r2 = new GreetingRunnable("Goodbye");
+		Thread t1 = new Thread(r1);
+		Thread t2 = new Thread(r2);
+		t1.start();
+		t2.start();
+	}
+}
