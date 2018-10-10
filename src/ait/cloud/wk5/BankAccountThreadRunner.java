60e3f2d687d1593779a6fc0db38f865cb38f1e97
Joe O'Regan <joeaoregan@yahoo.ie>
2018-10-10 16:45:14 +0100

    Bank Account: Thread locking

--- src/ait/cloud/wk5/BankAccountThreadRunner.java
+++ src/ait/cloud/wk5/BankAccountThreadRunner.java
@@ -4,27 +4,22 @@
 */
 package ait.cloud.wk5;
 
-public class BankAccountThreadRunner
-{
-   public static void main(String[] args)
-   {
-      BankAccount account = new BankAccount();
-      final double AMOUNT = 100;
-      final int REPETITIONS = 100;
-      final int THREADS = 100;
+public class BankAccountThreadRunner {
+	public static void main(String[] args) {
+		BankAccount account = new BankAccount();
+		final double AMOUNT = 100;
+		final int REPETITIONS = 10000;
+		final int THREADS = 100;
 
-      for (int i = 1; i <= THREADS; i++)
-      {
-         DepositRunnable d = new DepositRunnable(
-            account, AMOUNT, REPETITIONS);
-         WithdrawRunnable w = new WithdrawRunnable(
-            account, AMOUNT, REPETITIONS);
-         
-         Thread dt = new Thread(d);
-         Thread wt = new Thread(w);
-         
-         dt.start();
-         wt.start();
-      }
-   }
+		for (int i = 1; i <= THREADS; i++) {
+			DepositRunnable d = new DepositRunnable(account, AMOUNT, REPETITIONS);
+			WithdrawRunnable w = new WithdrawRunnable(account, AMOUNT, REPETITIONS);
+
+			Thread dt = new Thread(d);
+			Thread wt = new Thread(w);
+
+			dt.start();
+			wt.start();
+		}
+	}
 }
